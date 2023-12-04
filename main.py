8896d6d64d4fded98da598adfcbaf059eeb95c57
Mathis Zerbib <33841749+MathisZerbib@users.noreply.github.com>
2023-12-04 20:32:59 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 import together
-from langchain.chains.conversation.memory import ConversationSummaryMemory
+from langchain.chains.conversation.memory import ConversationBufferMemory
 from langchain.chains import ConversationChain
 from langchain.prompts.prompt import PromptTemplate
 import logging
@@ -25,116 +25,16 @@ from langchain.llms.base import LLM
 from langchain.llms.utils import enforce_stop_tokens
 from langchain.utils import get_from_dict_or_env
 
+# Import Flask modules
+from flask import Flask, request, jsonify
+
 class TogetherLLM(LLM):
-    """Together large language models."""
-
-    model: str = "togethercomputer/llama-2-70b-chat"
-    """model endpoint to use"""
-
-    together_api_key: str = "bcb47299a331e5736edb40b846e0b6f9654842e1e64faeaacc624e97244f9a89"
-    """Together API key"""
-
-    temperature: float = 0.7
-    """What sampling temperature to use."""
-
-    max_tokens: int = 512
-    """The maximum number of tokens to generate in the completion."""
-
-
-    class Config:
-        extra = Extra.forbid
-
-    @root_validator()
-    def validate_environment(cls, values: Dict) -> Dict:
-        """Validate that the API key is set."""
-        api_key = get_from_dict_or_env(
-            values, "together_api_key", "TOGETHER_API_KEY"
-        )
-        values["together_api_key"] = api_key
-        return values
-
-    @property
-    def _llm_type(self) -> str:
-        """Return type of LLM."""
-        return "together"
-
-    def _call(
-        self,
-        prompt: str,
-        **kwargs: Any,
-    ) -> str:
-        """Call to Together endpoint."""
-        together.api_key = self.together_api_key
-        output = together.Complete.create(prompt,
-                                          model=self.model,
-                                          max_tokens=self.max_tokens,
-                                          temperature=self.temperature,
-                                          stop=["<|im_end|>","Human:" ],
-                                          )
-        text = output['output']['choices'][0]['text']
-        return text
-
-llm = TogetherLLM(
-    model= 'Open-Orca/Mistral-7B-OpenOrca',
-    temperature = 0.9,
-
-    max_tokens = 624
-)
-from langchain.chat_models import ChatOpenAI
-from langchain.prompts import (
-    ChatPromptTemplate,
-    MessagesPlaceholder,
-    SystemMessagePromptTemplate,
-    HumanMessagePromptTemplate,
-)
-from langchain.chains import LLMChain
-from langchain.memory import ConversationBufferMemory
-
-
-llm = llm
-prompt = ChatPromptTemplate(
-    messages=[
-        SystemMessagePromptTemplate.from_template(
-            "You are a nice chatbot having a conversation with a human."
-        ),
-        # The `variable_name` here is what must align with memory
-        MessagesPlaceholder(variable_name="chat_history"),
-        HumanMessagePromptTemplate.from_template("{question}")
-    ]
-)
-# Notice that we `return_messages=True` to fit into the MessagesPlaceholder
-# Notice that `"chat_history"` aligns with the MessagesPlaceholder name.
-memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
-conversation = LLMChain(
-    llm=llm,
-    prompt=prompt,
-    verbose=True,
-    memory=memory
-)
-#m=conversation.predict(question="how are you today")
-#print(m)
-def res():
-    memory.clear()
-def predict(que):
-    k = conversation.predict(question=que)
-    print(k)
-    lines = k.split('\n')
-    chatbot_value = k  # Default to the full response if 'AI:' is not found
-    ai_lines = [line.split('AI: ', 1)[-1].strip() for line in lines if 'AI:' in line]
-    
-    # Print and return only unique 'AI:' lines
-    for ai_line in set(ai_lines):
-        print(ai_line)
-        chatbot_value = ai_line
-    
-    return chatbot_value
-
-#k=predict("when USA got independence")
-#print(k)
-from flask import Flask, request
-
+    # ... (your existing TogetherLLM code)
 
 app = Flask(__name__)
+
+# Global variable for conversation history
+conversation_history = []
 
 @app.route('/')
 def hello():
@@ -142,23 +42,53 @@ def hello():
 
 @app.route('/post_example', methods=['POST'])
 def post_example():
+    global conversation_history
+
     if request.method == 'POST':
-        # Access the data sent in the POST request
-        data = request.get_json()  # Assuming the data is sent as JSON
-        # You can also use request.form to get form data
-        # data = request.form
+        try:
+            # Ensure that the data is in JSON format
+            data = request.get_json()
+            if not data:
+                raise ValueError("Invalid JSON format")
 
-        # Process the data
-        # For example, if the JSON contains a key 'message'
-        if 'question' in data:
-            received_message = data['question']
-            o=predict(received_message)
-
-            return f"Received message: {o}"
-        else:
-            return "No 'message' key found in the POST request data"
+            # Process the data
+            if 'question' in data:
+                received_message = data['question']
+                conversation_history.append(received_message)
+                o = predict(received_message, conversation_history)
+                return jsonify({"received_message": received_message, "response": o})
+            else:
+                return jsonify({"error": "No 'question' key found in the JSON data"})
+        except Exception as e:
+            return jsonify({"error": str(e)})
     else:
-        return "This endpoint only accepts POST requests"
+        return jsonify({"error": "This endpoint only accepts POST requests"})
+
+def predict(que, conversation_history):
+    global llm  # Use the LLM instance globally
+
+    # Debug print to check conversation history
+    print("Conversation History:", conversation_history)
+
+    # Use conversation_history in LLMChain or other relevant places
+    # For example, you may want to clear memory before making a new prediction
+    llm.memory.clear()
+    
+    k = llm.predict(question=que)
+    print(k)
+    lines = k.split('\n')
+    chatbot_value = k  # Default to the full response if 'AI:' is not found
+    ai_lines = [line.split('AI: ', 1)[-1].strip() for line in lines if 'AI:' in line]
+    
+    # Debug print to check AI lines
+    print("AI Lines:", ai_lines)
+
+    # Print and return only unique 'AI:' lines
+    for ai_line in set(ai_lines):
+        print(ai_line)
+        chatbot_value = ai_line
+
+    return chatbot_value
 
 # if __name__ == '__main__':
 #    app.run(debug=True)
