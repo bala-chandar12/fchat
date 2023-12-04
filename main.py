acffb1fa77d01227a602a9f12df02d9a6c4ea2a1
Mathis Zerbib <33841749+MathisZerbib@users.noreply.github.com>
2023-12-04 11:58:35 +0100

    Update main.py

--- main.py
+++ main.py
@@ -157,4 +157,5 @@ def post_example():
         return "This endpoint only accepts POST requests"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+#    app.run(debug=True)
+app.run(debug=True, port=os.getenv("PORT", default=5000))
