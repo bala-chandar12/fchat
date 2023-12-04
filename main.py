799c9ce855e09ee66e97fb1441f74aa4d4d89e45
Balachandar <105204404+bala-chandar12@users.noreply.github.com>
2023-12-04 08:17:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -110,20 +110,51 @@ conversation = LLMChain(
     verbose=True,
     memory=memory
 )
-m=conversation.predict(question="how are you today")
-print(m)
+#m=conversation.predict(question="how are you today")
+#print(m)
 def res():
     memory.clear()
 def predict(que):
     k= conversation.predict(question=que)
     print(k)
     lines = k.split('\n')
-    chatbot_value="no response"
+    chatbot_value=k
     for line in lines:
         if 'AI:' in line:
             # Extracting the value after 'Chatbot:' by removing the 'Chatbot: ' part
             chatbot_value = line.split('AI: ')[1].strip()
             print(chatbot_value)
     return chatbot_value
-k=predict("when USA got independence")
-print(k)
+#k=predict("when USA got independence")
+#print(k)
+from flask import Flask, request
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Hello, welcome to my Flask server!'
+
+@app.route('/post_example', methods=['POST'])
+def post_example():
+    if request.method == 'POST':
+        # Access the data sent in the POST request
+        data = request.get_json()  # Assuming the data is sent as JSON
+        # You can also use request.form to get form data
+        # data = request.form
+
+        # Process the data
+        # For example, if the JSON contains a key 'message'
+        if 'question' in data:
+            received_message = data['question']
+            o=predict(received_message)
+
+            return f"Received message: {o}"
+        else:
+            return "No 'message' key found in the POST request data"
+    else:
+        return "This endpoint only accepts POST requests"
+
+if __name__ == '__main__':
+    app.run(debug=True)
